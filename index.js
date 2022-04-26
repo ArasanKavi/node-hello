598d83f490b706815f7048a52c4565431c1ff87a
ArasanKavi <101690980+ArasanKavi@users.noreply.github.com>
2022-04-26 19:31:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3055;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello\n'
+  const msg = 'He\n'
   res.end(msg);
 });
 
